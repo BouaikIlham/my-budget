0d35181290ee5315588ab9fe682e36e28a81d5b5
ilham bouaik <ibouaik@gmail.com>
2022-09-26 17:33:21 +0100

    add valisates

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :categories
   has_many :transactions
+  validates :measurement_unit, presence: true
 end
