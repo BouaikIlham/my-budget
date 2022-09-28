4a4ffc340f1736f2e708f9533d013c8d2de73814
ilham bouaik <ibouaik@gmail.com>
2022-09-28 10:28:27 +0100

    destroy category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -24,6 +24,13 @@ def create
     end
   end
 
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path, notice: 'category was successfully Deleted.'
+  end
+
+
 
 private
 
