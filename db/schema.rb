85de9981637d6cd10606b688a64f328119fab18a
ilham bouaik <ibouaik@gmail.com>
2022-09-28 17:29:18 +0100

    edite database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_26_164128) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_28_162830) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,14 +23,21 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_26_164128) do
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
+  create_table "categories_transactions", force: :cascade do |t|
+    t.bigint "categories_id", null: false
+    t.bigint "transactions_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["categories_id"], name: "index_categories_transactions_on_categories_id"
+    t.index ["transactions_id"], name: "index_categories_transactions_on_transactions_id"
+  end
+
   create_table "transactions", force: :cascade do |t|
     t.string "name"
     t.float "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.bigint "category_id", null: false
-    t.index ["category_id"], name: "index_transactions_on_category_id"
     t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
@@ -48,6 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_26_164128) do
   end
 
   add_foreign_key "categories", "users"
-  add_foreign_key "transactions", "categories"
+  add_foreign_key "categories_transactions", "categories", column: "categories_id"
+  add_foreign_key "categories_transactions", "transactions", column: "transactions_id"
   add_foreign_key "transactions", "users"
 end
